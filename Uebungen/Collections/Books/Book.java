eec66a6d99401a24f2d94d7755bc7cf2676060f6
Tobias Ebbers <tobias@ebbers.biz>
2017-05-18 21:11:46 +0200

    :trollface: Methode part6 done :trollface:

--- Uebungen/Collections/Books/Book.java
+++ Uebungen/Collections/Books/Book.java
@@ -1,10 +1,10 @@
 package Books;
 
-public class Book {
+public class Book implements Comparable<Book> {
 	private String author;
 	private String title;
 	private int issue;
-	
+
 	public Book(String author, String title, int issue) {
 		setAuthor(author);
 		setTitle(title);
@@ -33,5 +33,27 @@ public class Book {
 
 	public void setIssue(int issue) {
 		this.issue = issue;
+	}
+
+	@SuppressWarnings("null")
+	@Override
+	public int compareTo(Book o) {
+		if ((null == o) && (o.getClass() != getClass())) {
+			return -1;
+		}
+
+		int i = author.compareTo(o.getAuthor());
+
+		if (i != 0) {
+			return i;
+		} else {
+			i = title.compareTo(o.getTitle());
+			if (i != 0) {
+				return i;
+			} else {
+				i = issue - o.issue;
+				return i;
+			}
+		}
 	}
 }
--- Uebungen/Collections/Books/Exercise.java
+++ Uebungen/Collections/Books/Exercise.java
@@ -3,10 +3,12 @@ package Books;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Exercise {
 
 	public static Book[] books = new Book[6];
+	public static int maxLenAuthor = 0, maxLenTitle = 0, maxLenIssue = 0;
 
 	public Exercise() {
 		books[0] = new Book("Goethe", "Faust I", 20_000);
@@ -15,8 +17,26 @@ public class Exercise {
 		books[3] = new Book("Stephen Hawking", "Eine kurze Geschichte der Zeit", 64_000);
 		books[4] = new Book("Mario Puzo", "Der Pate", 128_000);
 		books[5] = new Book("Fontane", "Effi Briest", 10_000);
-	}
+		// Get Max Lengths;
+		for (Book b : books) {
+			maxLenAuthor = (b.getAuthor().length() > maxLenAuthor) ? b.getAuthor().length() : maxLenAuthor;
+			maxLenTitle = (b.getTitle().length() > maxLenTitle) ? b.getTitle().length() : maxLenTitle;
+			int issueLen = String.valueOf(b.getIssue()).length();
+			maxLenIssue = (issueLen > maxLenIssue) ? issueLen : maxLenIssue;
+		}
 
+	}
+	
+	private static void printArrayList(ArrayList<Book> l){
+		String[] spaces = {null, null, null};
+		for(Book b : l){
+			spaces[0] = new String(new char[maxLenAuthor - b.getAuthor().length() + 1]).replace("\0", " ");
+			spaces[1] = new String(new char[maxLenTitle - b.getTitle().length()+1]).replace("\0", " ");
+			spaces[2] = new String(new char[maxLenIssue - String.valueOf(b.getIssue()).length() +1]).replace("\0", " ");
+			System.out.println(b.getAuthor()+spaces[0]+"\""+b.getTitle()+"\""+spaces[1]+"Auflage: "+b.getIssue()+spaces[2]+"Stueck");
+		}
+	}
+	
 	@SuppressWarnings("unused")
 	private static void part6() {
 		ArrayList<Book> bookList = new ArrayList<Book>(6);
@@ -24,9 +44,13 @@ public class Exercise {
 			bookList.add(b);
 		}
 		//Ausgabe der Liste unsortiert
-		for(Book b : bookList){
-			System.out.println(b.getAuthor()+":\t\""+b.getTitle()+"\""+"\t"+"Auflage: "+b.getIssue()+" Stueck");
-		}
+		printArrayList(bookList);	
+		System.out.println("\n*** in umgekehrter Reihenfolge ***\n");
+		Collections.reverse(bookList);
+		printArrayList(bookList);
+		System.out.println("\n*** in sortierter, umgekehrter Reihenfolge ***\n");
+		Collections.sort(bookList);
+		printArrayList(bookList);
 	};
 
 	private static void part7() {
@@ -56,8 +80,8 @@ public class Exercise {
 		if (args.length == 1) {
 			callMethod(args[0]);
 		} else {
-			System.out.println(
-					"Programm erwartet 1 Argument: \n6 - Ausgabe als ArrayList\n7 - HashMap Implementierung\n8 - Set Implementierung");
+			System.out.println("Programm erwartet 1 Argument: " + "\n6 - Ausgabe als ArrayList"
+					+ "\n7 - HashMap Implementierung" + "\n8 - Set Implementierung");
 		}
 	}
 
