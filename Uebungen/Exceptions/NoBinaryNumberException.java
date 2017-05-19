143a9ee912391f38ba8053475486c71808de608e
Tobias Ebbers <tobias@ebbers.biz>
2017-05-19 12:20:49 +0200

    Exceptions Uebungen

---
+++ Uebungen/Exceptions/NoBinaryNumberException.java
@@ -0,0 +1,17 @@
+public class NoBinaryNumberException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public NoBinaryNumberException() {
+		super();
+	}
+
+	public NoBinaryNumberException(String message) {
+		super(message);
+	}
+	
+	public NoBinaryNumberException(String binary, int position) {
+	}
+
+
+}
