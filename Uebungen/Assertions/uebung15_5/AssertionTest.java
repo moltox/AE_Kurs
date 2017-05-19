e7482286bc8c52ece6f51438017fe44964077dd3
Tobias Ebbers <tobias@ebbers.biz>
2017-05-19 23:33:25 +0200

    Assertion Uebung

--- Uebungen/Assertions/uebung15_5/AssertionTest.java
+++ Uebungen/Assertions/uebung15_5/AssertionTest.java
@@ -4,6 +4,8 @@ public class AssertionTest {
 
 	public static void main(String[] args) {
 		assert (args.length == 2) : "Programmaufruf muss mit 2 Parametern erfolgen!";
+		//System.out.println("Arg 1: "+ args[0]);
+		//System.out.println("Arg 2: "+ args[1]);
 		System.out.println("Assertion Test gelaufen");
 	}
 
