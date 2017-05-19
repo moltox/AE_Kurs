f1afecbac41177ff5759acf6e018b96982410365
Tobias Ebbers <tobias@ebbers.biz>
2017-05-19 12:48:41 +0200

    foo

---
+++ Uebungen/Exceptions/uebung14_7/ConvertBinary.java
@@ -0,0 +1,7 @@
+package uebung14_7;
+
+public class ConvertBinary {
+	public static int parseBinary(String binStr) {
+		return 0;
+	}
+}
---
+++ Uebungen/Exceptions/uebung14_7/ConvertBinaryTest.java
@@ -0,0 +1,9 @@
+package uebung14_7;
+
+public class ConvertBinaryTest {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
