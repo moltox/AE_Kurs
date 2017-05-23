bda417a723742fa84bacee4fa9f4144056c2487a
Tobias Ebbers <tobias@ebbers.biz>
2017-05-23 15:57:15 +0200

    Uebung 17.10 Nummer 1 Done !

--- Uebungen/Streams/uebung_17_10/Measurements.java
+++ Uebungen/Streams/uebung_17_10/Measurements.java
@@ -1,29 +1,28 @@
 package uebung_17_10;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Measurements {
 
 	public static void main(String[] args) throws IOException {
+		BufferedReader bufIn = new BufferedReader(new FileReader("Streams\\uebung_17_10\\valuesIn.dat"));
+		BufferedWriter bufOut = new BufferedWriter(new FileWriter("Streams\\uebung_17_10\\valuesOut.dat"));
 		String s;
-		BufferedReader bfReader = null;
-		bfReader = new BufferedReader(new FileReader("Streams\\uebung_17_10\\valuesIn.dat"));
-		while( ( s = bfReader.readLine()) != null) {
-			System.out.println(s);
+		int messreihen = 20, messwerte = 10;
+		int currentRead;
+		for (int c = 0; c < messreihen; c++) {
+			for (int cc = 0; cc < messwerte; cc++) {
+				currentRead = bufIn.read();
+				bufOut.write(currentRead);
+				System.out.printf("%4d", currentRead);
+			}
+			System.out.printf(" Ende der Messreihe %d\n", c);
 		}
-		
-		FileReader fRead = new FileReader("Streams\\uebung_17_10\\valuesIn.dat");
-		int t = 0;
-		while( ( t = fRead.read()) != -1) {
-			System.out.println(t);
-		}
-		
-		
-		bfReader.close();
-
+		bufIn.close();
+		bufOut.close();
 	}
-
 }
