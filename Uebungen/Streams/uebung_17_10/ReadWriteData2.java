00b18b44192312060875611268ad5d0390bc1eac
Tobias Ebbers <tobias@ebbers.biz>
2017-05-24 08:49:30 +0200

    ReadWrite mit read()

---
+++ Uebungen/Streams/uebung_17_10/ReadWriteData2.java
@@ -0,0 +1,75 @@
+package uebung_17_10;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ReadWriteData2 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufRead = new BufferedReader(new InputStreamReader(System.in));
+		ArrayList<List<String>> eingaben = new ArrayList<List<String>>();
+
+		boolean moreData = true;
+		char command = '\0';
+		String eingabeAufforderung[] = { "Name: ", "Geschlecht: ", "Größe(in cm): ", "Gewicht(in kg): " };
+
+		while (moreData) {
+			System.out.println("Daten eingeben (J)a / (N)ein ?");
+			command =(char) bufRead.read();
+			
+			if (command == 'j' || command == 'J') {
+				List<String> in = new LinkedList<String>();
+
+				for (int c = 0; c < eingabeAufforderung.length; c++) {
+					
+					System.out.print(eingabeAufforderung[c]);
+					in.add(bufRead.readLine());
+				}
+				eingaben.add(in);
+			} else if (command == 'n' || command == 'N') {
+				System.out.println("Beende und Speicher Daten!");
+				moreData = false;
+			} else {
+				continue;
+			}
+		} ;
+		bufRead.close();
+	/*	
+		BufferedWriter bufWrite = new BufferedWriter(new FileWriter("Streams\\uebung_17_10\\personal.csv"));
+		
+		char delimiter = ',';
+		bufWrite.write("sep=,");
+		bufWrite.newLine();
+		for (List<String> al : eingaben) {
+			Iterator<String> it = al.iterator();
+
+			while (it.hasNext()) {
+				bufWrite.write("\""+it.next()+"\"" + delimiter);
+			}
+			bufWrite.newLine();
+		
+		}
+		bufWrite.close();
+		*/
+		PrintWriter pWriter = new PrintWriter(new FileWriter("Streams\\uebung_17_10\\personal.csv"));
+		char delimiter = '|';
+		pWriter.write("sep=|");
+		pWriter.println();
+		for(List<String> listen : eingaben){
+			Iterator<String> iter = listen.iterator();
+			while(iter.hasNext()){
+				pWriter.print("\""+iter.next()+"\"" + delimiter);
+			}
+			pWriter.println();
+		}
+		pWriter.close();
+	}
+}
