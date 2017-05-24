63932fd2e089fc84d75a9b90dcd56cf2e041c480
Tobias Ebbers <tobias@ebbers.biz>
2017-05-24 23:26:20 +0200

    Uebung 2 Zeit messen done

--- Uebungen/Sonstiges/NuetzlicheKlassenUndPackages/uebung_18_10/uebung_2/TimeMeasurement.java
+++ Uebungen/Sonstiges/NuetzlicheKlassenUndPackages/uebung_18_10/uebung_2/TimeMeasurement.java
@@ -1,9 +1,28 @@
 package NuetzlicheKlassenUndPackages.uebung_18_10.uebung_2;
 
+import java.time.Instant;
+
 public class TimeMeasurement {
 
 	public static void main(String[] args) {
+		String s1 = "X";
+		StringBuffer sBuf = new StringBuffer("X");
 
+		int appendCount = 10_000;
+		long start, end;
+		start = System.nanoTime();
+		for (int c = 0; c < appendCount; c++) {
+			s1 += "X";
+		}
+		end = System.nanoTime();
+
+		System.out.println("String append: " + ((end - start) / 1_000_000) + "ms");
+		start = System.nanoTime();
+		for(int c = 0; c < appendCount; c++){
+			sBuf.append('X');
+		}
+		end = System.nanoTime();
+		System.out.println("StringBuffer append: " + ((end-start)/ 1_000_000) + "ms");
 	}
 
 }
