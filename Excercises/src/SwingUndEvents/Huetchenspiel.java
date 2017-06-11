32b14e5d09ac1e3c112a9c0b9fe2aa10bbf523b7
Admin <bkerkes>
2017-06-11 22:52:21 +0200

    Huetchenspiel hinzugefuegt

--- Excercises/src/SwingUndEvents/Huetchenspiel.java
+++ Excercises/src/SwingUndEvents/Huetchenspiel.java
@@ -1,7 +1,5 @@
 package SwingUndEvents;
 
-import java.util.Random;
-
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.GridBagConstraints;
@@ -15,34 +13,32 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-@SuppressWarnings( "serial" )
-public class ZahlenRateSpiel extends JFrame implements ActionListener {
-	
-	// deklariere GUI - Elemente
+@SuppressWarnings("serial")
+public class Huetchenspiel extends JFrame implements ActionListener {
+		
 	private JFrame frame;
 	
 	private Container contentPane;
 	
-	private JLabel nameLabel;
+	private JLabel playerName;
 	private JTextField nameInput;
 	
-	private JLabel numberLabel;
-	private JTextField numberInput;
+	private JButton coneOne;
+	private JButton coneTwo;
+	private JButton coneThree;
 	
-	private JTextField eventOutput;
-		
-	private JButton btnNewGame;
-	private JButton btnConfirm;
-	private JButton btnBestOf;
-	private JButton btnDispose;
+	private JButton statistics;
+	private JButton dispose;
 	
-	// für die Zufallszahl und die Anzahl der Rateversuche
+	private JTextField event;
+	
 	private static int randomNumber;
 	private static int counter;
 	private static Random rd;
@@ -75,7 +71,7 @@ public class ZahlenRateSpiel extends JFrame implements ActionListener {
 	private static int erzeugeZufallszahl( ) {
 		
 		rd = new Random( );
-		int zufallszahl = rd.nextInt( 1000 );
+		int zufallszahl = rd.nextInt( 3 ) + 1;
 		System.out.println( zufallszahl );
 		return zufallszahl;		
 	}
@@ -90,31 +86,28 @@ public class ZahlenRateSpiel extends JFrame implements ActionListener {
 			System.err.println( e.toString( ) );
 		}
 	}
-	
 	public void readOutOfFile( ) {
 		
 		try (BufferedReader br = new BufferedReader( new FileReader( "winner.txt" ) ) ) {
 			
 			String inputString;
 			char wanted = '-';
-			String name = "";
-			int score = Integer.MAX_VALUE;
+			int counter = 0;
+			double score = 0.0;
 			
 			while ( ( inputString = br.readLine( ) ) != null ) {
 	            
+				counter++;
 				for ( int i = 0; i < inputString.length( ); i++ ) {
 	            	
 	            	if( inputString.charAt( i ) == wanted ) {
 	            		
-	            		if ( Integer.parseInt( inputString.substring( i + 2, inputString.length( ) ) ) < score ) {
-	            			
-	            			score = Integer.parseInt( inputString.substring( i + 2, inputString.length( ) ) );
-	            			name = inputString.substring( 0, i - 1 );
-	            		}	            		
+	            		score += Double.parseDouble( inputString.substring( i + 2, inputString.length( ) ) );	            			            		
 	            	}
 	            }
 	        }
-			eventOutput.setText("Rekordhalter: " + name + " mit " + score + " Versuchen");
+			score /= counter;
+			event.setText("Durchschnittliche Anzahl: "  + score );
 		} catch ( FileNotFoundException e ) {
 			
 			System.err.println( e.toString( ) );
@@ -127,39 +120,70 @@ public class ZahlenRateSpiel extends JFrame implements ActionListener {
 	@Override
 	public void actionPerformed( ActionEvent evt ) {
 		
-		incrementCounter( );
-		if ( evt.getActionCommand( ).equals( "Neues Spiel" ) ) {
+		if ( evt.getActionCommand( ).equals( "Huetchen 1" ) ) {
 			
-			setRandomNumber( );
-			resetCounter( );
-		}
-		if ( evt.getActionCommand( ).equals( "OK" ) ) {
-			
-			if ( Integer.parseInt( numberInput.getText( ) ) > getRandomNumber( ) ) {
+			incrementCounter( );
+			int match = 1;
+			if ( match == getRandomNumber( ) ) {
 				
-				eventOutput.setText( "Versuch Nr. " + getCounter( ) + ": " + numberInput.getText( ) + " zu hoch" );
-			}
-			else if( Integer.parseInt( numberInput.getText( ) ) < getRandomNumber( ) ) {
-				
-				eventOutput.setText( "Versuch Nr. " + getCounter( ) + ": " + numberInput.getText( ) + " zu klein" );
-			}
-			else if ( Integer.parseInt( numberInput.getText( ) ) == getRandomNumber( ) ) {
-				
-				eventOutput.setText( "Gewonnen! Versuch Nr. " + getCounter( ) + ": " + numberInput.getText( ) + " ist richtig!" );
+				event.setText( "Versuch " + getCounter( ) + " Gewonnen: Die Kugel war unter Hut 1");
 				writeInFile( );
+				resetCounter( );
+				setRandomNumber( );
+			}
+			else {
+				
+				event.setText( "Versuch " + getCounter( ) + " Falsch: Die Kugel war unter Hut " + getRandomNumber( ) );
+				setRandomNumber( );
 			}
 		}
-		if ( evt.getActionCommand( ).equals( "Bester" ) ) {
+		
+		if ( evt.getActionCommand( ).equals( "Huetchen 2" ) ) {
 			
+			incrementCounter( );
+			int match = 2;
+			if ( match == getRandomNumber( ) ) {
+				
+				event.setText( "Versuch " + getCounter( ) + " Gewonnen: Die Kugel war unter Hut 2");
+				writeInFile( );
+				resetCounter( );
+				setRandomNumber( );
+			}
+			else {
+				
+				event.setText( "Versuch " + getCounter( ) + " Falsch: Die Kugel war unter Hut " + getRandomNumber( ) );
+				setRandomNumber( );
+			}
+		}
+		
+		if ( evt.getActionCommand( ).equals( "Huetchen 3" ) ) {
+	
+			incrementCounter( );
+			int match = 3;
+			if ( match == getRandomNumber( ) ) {
+				
+				event.setText( "Versuch " + getCounter( ) + " Gewonnen: Die Kugel war unter Hut 3");
+				writeInFile( );
+				resetCounter( );
+				setRandomNumber( );
+			}
+			else {
+				
+				event.setText( "Versuch " + getCounter( ) + " Falsch: Die Kugel war unter Hut " + getRandomNumber( ) );
+				setRandomNumber( );
+			}
+		}
+		
+		if ( evt.getActionCommand( ).equals( "Statistik" ) ) {
+	
 			readOutOfFile( );
 		}
-		if ( evt.getActionCommand( ).equals( "Beenden" ) ) {
-			
+		if ( evt.getActionCommand( ).equals( "Exit" ) ) {
+	
 			System.exit( 0 );
 		}
 	}
-	
-	// zum setzen der "Einschränkungen" für die GUI - Elemente (für die "Schreibfaulen")
+		
 	static void addComponent ( 	Container cont, 
 								GridBagLayout gbl, 
 								Component c, 
@@ -179,9 +203,8 @@ public class ZahlenRateSpiel extends JFrame implements ActionListener {
 		cont.add( c );
 	}
 	
-	ZahlenRateSpiel( ) {
+	Huetchenspiel( ) {
 		
-		// baue den JFrame auf
 		frame = new JFrame( );
 		setTitle( "Ratespiel" );
 		
@@ -194,48 +217,48 @@ public class ZahlenRateSpiel extends JFrame implements ActionListener {
 		
 		// erzeuge Objektreferenzen für die einzelnen GUI - Elemente
 		
-		nameLabel = new JLabel( "Spielername:" );
+		playerName = new JLabel( "Spielername: " );
 		nameInput = new JTextField( );
 		
-		numberLabel = new JLabel( "Zahl zwischen 1 und 1000 eingeben:" );
-		numberInput = new JTextField( );
+		coneOne = new JButton( "Huetchen 1" );
+		coneOne.addActionListener( this );
 		
-		btnNewGame = new JButton( "Neues Spiel" );
-		btnNewGame.addActionListener( this );
+		coneTwo = new JButton( "Huetchen 2" );
+		coneTwo.addActionListener( this );
 		
-		btnConfirm = new JButton( "OK" );
-		btnConfirm.addActionListener( this );
+		coneThree = new JButton( "Huetchen 3" );
+		coneThree.addActionListener( this );
 		
-		btnBestOf = new JButton( "Bester" );
-		btnBestOf.addActionListener( this );
+		statistics = new JButton( "Statistik" );
+		statistics.addActionListener( this );
 		
-		btnDispose = new JButton( "Beenden" );
-		btnDispose.addActionListener( this );
+		dispose = new JButton( "Exit" );
+		dispose.addActionListener( this );
 		
-		eventOutput = new JTextField( );
+		event = new JTextField( );
 		
 		// baue die GUI auf über die statische addComponent - Methode auf
 		// 													x  y  w  h  wx wy
-		addComponent( contentPane, gbl, nameLabel, 			0, 0, 1, 1, 1, 0);
-		addComponent( contentPane, gbl, nameInput, 			1, 0, 3, 1, 1, 0);
-		addComponent( contentPane, gbl, numberLabel,		0, 1, 1, 1, 1, 0);
-		addComponent( contentPane, gbl, numberInput,		1, 1, 3, 1, 1, 0);
-		addComponent( contentPane, gbl, btnNewGame,			0, 2, 1, 1, 1, 0);
-		addComponent( contentPane, gbl, btnConfirm,			1, 2, 1, 1, 1, 0);
-		addComponent( contentPane, gbl, btnBestOf, 			2, 2, 1, 1, 1, 0);
-		addComponent( contentPane, gbl, btnDispose,			3, 2, 1, 1, 1, 0);
-		addComponent( contentPane, gbl, eventOutput,		0, 3, 4, 1, 1, 0);
+		addComponent( contentPane, gbl, playerName, 		0, 0, 1, 1, 1, 0);
+		addComponent( contentPane, gbl, nameInput, 			1, 0, 2, 1, 1, 0);
+		addComponent( contentPane, gbl, coneOne,			0, 1, 1, 1, 1, 0);
+		addComponent( contentPane, gbl, coneTwo,			1, 1, 1, 1, 1, 0);
+		addComponent( contentPane, gbl, coneThree,			2, 1, 1, 1, 1, 0);
+		addComponent( contentPane, gbl, event,				0, 2, 3, 1, 1, 0);
+		addComponent( contentPane, gbl, statistics,			1, 3, 1, 1, 1, 0);
+		addComponent( contentPane, gbl, dispose, 			2, 3, 1, 1, 1, 0);
+
 		
 		add( contentPane );
-		setSize( 500, 125 );
+		setSize( 400, 130 );
 		setVisible( true );
 		
-		// erzeuge 1. Zufallszahl
 		setRandomNumber( );
+		resetCounter( );
 	}
-	
-	public static void main( String[ ] args ) {
 
-		new ZahlenRateSpiel( );
+	public static void main( String[ ] args ) {
+		
+		new Huetchenspiel( );
 	}
 }
