d7aa6f1dbd350bbef669ecf321c9d510e6eeb955
Anwender <Benjamin.Kerkes@web.de>
2017-06-15 17:12:37 +0200

    noch nicht fertig!

---
+++ Excercises/src/SwingUndEvents/DateiAnzeiger.java
@@ -0,0 +1,84 @@
+package SwingUndEvents;
+
+import java.awt.GridLayout;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+
+public class DateiAnzeiger {
+	
+	private JFileChooser fc;
+	private JFrame frame;
+	private JTextArea tarea;
+	private JPanel panel;
+	private JScrollPane scrollPane;
+	
+	private int option;
+	
+	private void erzeugeFensterUndLeseDateiAus( File file ) {
+		
+		String inputString = "";
+		int counter = 0;
+		
+		frame = new JFrame( );
+		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		panel = new JPanel( );
+		panel.setLayout( new GridLayout( ) );
+		tarea = new JTextArea( );
+		scrollPane = new JScrollPane( JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+                JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS );
+		scrollPane.setViewportView( tarea );
+				
+		try (BufferedReader br = new BufferedReader( new FileReader( file.getAbsolutePath( ) ) ) ) {
+			
+			while ( ( inputString = br.readLine( ) ) != null || counter < 10) {
+				
+				inputString += "\n";
+				counter++;
+				tarea.append( inputString );
+				tarea.setRows( counter + 1 );
+				
+			}
+		} catch ( FileNotFoundException e ) {
+			
+			System.err.println( e.toString( ) );
+		} catch ( IOException e ) {
+			
+			System.err.println( e.toString( ) );
+		}
+		panel.add( tarea );
+		frame.add( panel );
+		frame.setSize( 100, 100);
+		frame.setVisible( true );
+	}
+	
+	public static void main( String[ ] args ) {
+		
+		//erzeuge Referenz auf 'DateiAnzeiger'
+		DateiAnzeiger da = new DateiAnzeiger( );
+		
+		// erzeuge ein Dateiauswahldialog und übergebe den returnstate an 'option'
+		da.fc = new JFileChooser( );
+		da.option = da.fc.showOpenDialog( null );
+		
+		if ( da.option == JFileChooser.APPROVE_OPTION ) { // ging alles glatt ?
+			
+			//erzeuge FileObjekt und übergebe den zuvor ausgewählten Dateinamen
+			File file = da.fc.getSelectedFile( );
+			da.erzeugeFensterUndLeseDateiAus( file );
+		} else { // falls nicht
+			
+			System.out.println( "Auswahl abgebrochen" );
+			System.exit( 0 );
+		}
+	}
+}
--- Excercises/src/SwingUndEvents/Editor.java
+++ Excercises/src/SwingUndEvents/Editor.java
@@ -7,21 +7,30 @@ import javax.swing.JMenuItem;
 
 public class Editor extends JFrame {
 	
+	private static final long serialVersionUID = 1L;
 	private JFrame frame;
 	private JMenuBar mbar;
 	private JMenu menu1;
 	private JMenu menu2;
+	private JMenu menu3;
 	
-	Editor( ) {
+	Editor( ) { // Die Menus durch Methoden zu 'füllen' ist natürlich schöner
 		
 		frame = new JFrame( );
 		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 		
+		//erzeuge Obermenu
 		mbar = new JMenuBar( );
+		menu1 = new JMenu( "Datei" );
+		menu2 = new JMenu( "Bearbeiten" );
+		menu3 = new JMenu( "Senden an" );
+		mbar.add( menu1 );
+		mbar.add( menu2 );
+		//*******************************
 		
-		menu1 = new JMenu( );
-/*		menu1.add( new JMenuItem( "Neu..." ) );
-		menu1.add( "Oeffnen..." );
+		// fuelle das erste Menu(Datei) mit Eintraegen
+		menu1.add( new JMenuItem( "Neu..." ) );
+		menu1.add( "Öffnen..." );
 		menu1.add( "Schließen" );
 		menu1.addSeparator( );
 		menu1.add( "Speichern" );
@@ -35,18 +44,52 @@ public class Editor extends JFrame {
 		menu1.add( "Seitenansicht" );
 		menu1.add( "Drucken..." );
 		menu1.addSeparator( );
-		menu1.add( "Senden an" );
+		menu1.add( menu3 ); //erzeuge Untermenu
 		menu1.add( "Eigenschaften" );
 		menu1.addSeparator( );
 		menu1.add( "Beenden" );
-		*/
-		mbar.add( menu1 );
-		setJMenuBar( mbar );
-		setSize( 500, 500 );
-		setVisible( true );
+		// ***********************************************
 		
+		//fuelle das zweite Menu(Bearbeiten) mit Eintraegen
+		menu2.add( "Rückgaengig" );
+		menu2.add( "Wiederholen" );
+		menu2.addSeparator( );
+		menu2.add( "Auschneiden" );
+		menu2.add( "Kopieren" );
+		menu2.add( "Office-Zwischenablage" );
+		menu2.addSeparator( );
+		menu2.add( "Einfügen" );
+		menu2.add( "Inhalt einfügen" );
+		menu2.add( "Als Hyperlink einfügen" );
+		menu2.addSeparator( );
+		menu2.add( "Löschen" );
+		menu2.add( "Alles markieren" );
+		menu2.addSeparator( );
+		menu2.add( "Suchen..." );
+		menu2.add( "Ersetzen..." );
+		menu2.add( "Gehe zu..." );
+		menu2.addSeparator( );
+		menu2.add( "Verknüpfungen..." );
+		menu2.add( "Objekt" );
 		
+		//fülle das Untermenu(Senden an) mit Eintraegen
+		menu3.add( "E-Mail-Empänger" );
+		menu3.add( "E-Mail-Empänger(zur Überarbeitung)..." );
+		menu3.addSeparator( );
+		menu3.add( "E-Mail-Empänger(als Anlage)..." );
+		menu3.add( "Verteilerempfänger..." );
+		menu3.add( "Onlinebesprechungsteilnehmer" );
+		menu3.add( "Exchange-Ordner..." );
+		menu3.add( "Fax-Empfänger..." );
+		menu3.addSeparator( );
+		menu3.add( "Microsoft Powerpoint" );
+		//************************************************
 		
+		//füge die Menubar in den Frame ein, initialisere selbigen
+		frame.setJMenuBar( mbar );
+		frame.setSize( 500, 500 );
+		frame.setVisible( true );		
+		//***************************************************
 	}
 
 	public static void main( String[ ] args ) {
