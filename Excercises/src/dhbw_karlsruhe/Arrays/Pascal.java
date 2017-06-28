3bd0fc7e3d5d94a4919bb0f896019f4ba96da68a
bkerkes <Benjamin.Kerkes@web.de>
2017-06-28 09:54:51 +0200

    DHBW Aufgabe 8 Array fertig

--- Excercises/src/dhbw_karlsruhe/Arrays/Pascal.java
+++ Excercises/src/dhbw_karlsruhe/Arrays/Pascal.java
@@ -5,15 +5,27 @@ public class Pascal {
 	public static void main( String[ ] args ) {
 		
 		int[ ][ ] pascal = new int[ 10 ][ ];		
-		int anzahlElemente = 1;
 		
 		for ( int columnId = 0; columnId < pascal.length; columnId++ ) {
 			
 			pascal[ columnId ] = new int[ columnId + 1 ];
-			for ( int rowId = 0; rowId < anzahlElemente; rowId++ ) {
+			pascal[ columnId ][ 0 ] = 1;
+			pascal[ columnId ][ columnId ] = 1;
 			
-				pascal[ columnId ][ rowId ] = 
+			for ( int rowId = 1; rowId < columnId; rowId++ ) {
+			
+				pascal[ columnId ][ rowId ] = pascal [ columnId - 1 ][ rowId - 1 ] + pascal[ columnId - 1 ][ rowId ];
 			}			
+		}
+		
+		for ( int i=0; i  < pascal.length; i++ ) { 
+			
+			System.out.print( "Zeile = " + i + "\t" ); 
+			
+			for ( int j = 0; j < i + 1; j++ ) 			
+				System.out.print( pascal[ i ][ j ] + "\t" );
+			
+			System.out.print( "\n" );			
 		}
 	}
 }
